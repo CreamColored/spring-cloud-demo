259817a1e656b373d146ab80de28c79ad5a320e0
Stephen Wong <stephentheweirdo@gmail.com>
2020-11-04 10:38:11 +0800

    create spring cloud demo project

---
+++ test/src/main/java/com/amadeus/test/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.amadeus.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/one")
+    public String testOne() {
+        return "测试1";
+    }
+}
